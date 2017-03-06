a5dc43c38fd61a6e3a2b90fa7357e43697389e69
CoDeGeEk-root <vakasrikar@gmail.com>
2017-03-06 18:41:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 
 //change the text of main text
 
-var element = document.getelementbyId('main-text');
+var element = document.getElementById('main-text');
 
 element.innerHTML = "Wow, this is awesome mate!";

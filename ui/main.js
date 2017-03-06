62a318208f04f8a45e6bd210fce6be6badec6e00
CoDeGeEk-root <vakasrikar@gmail.com>
2017-03-06 18:54:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,13 @@ element.innerHTML = "Wow, this is awesome mate!";
 
 var img = document.getElementById('mady');
 
+var marginLeft = 0;
+
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px' ;
+}
+
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 100);
 };

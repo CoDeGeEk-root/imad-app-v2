c5e799b87dc994f390b88ecd678227517e551e25
CoDeGeEk-root <vakasrikar@gmail.com>
2017-03-06 18:47:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,11 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = "Wow, this is awesome mate!";
+
+//mave image
+
+var img = document.getElementById('mady');
+
+img.onclick = function () {
+    img.style.marginLeft = '100px';
+};

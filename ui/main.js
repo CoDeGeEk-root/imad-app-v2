764e3a67ade41f5be11a502e2baad29cb8819041
CoDeGeEk-root <vakasrikar@gmail.com>
2017-03-07 18:44:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,27 @@
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function () {
   
+  //request to counter
+  // This is request object
+  var request  = new XMLHttpRequest();
+  //check if the request is done and capture the response
+  //render this respnse to HTML file using counter variable.
+  request.onreadystatechange = function(){
+    if(request.readystate === XMLHttpRequest.DONE){
+        if(request.status === 200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+        
+    }
+  };
+  //Make the request
+  request.open('GET','codegeek-root.imad.hasura-app.io/counter',true);
+  request.send(null);
   
   
-  counter += 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+  
     
 };
